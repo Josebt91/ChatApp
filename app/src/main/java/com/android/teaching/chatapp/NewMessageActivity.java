7adf393ecb15a16b9093899c0740428379ca57aa
Josebt91 <josebt91@hotmail.com>
2018-06-07 13:59:06 +0200

    Avanzamos, por ahora vamos hasta el apartado 3b.

--- app/src/main/java/com/android/teaching/chatapp/NewMessageActivity.java
+++ app/src/main/java/com/android/teaching/chatapp/NewMessageActivity.java
@@ -1,7 +1,9 @@
 package com.android.teaching.chatapp;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class NewMessageActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class NewMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_message);
+
+        Toolbar myToolbar=findViewById(R.id.toolbar_new);
+        setSupportActionBar(myToolbar);
+
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
